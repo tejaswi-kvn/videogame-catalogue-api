d8f67d1d8dd1cf28f7ddf14691699560581bf9c8
tejaswi-kvn <tejaswi.kvn@gmail.com>
2026-01-16 00:53:25 -0500

    Update VideoGames.cs

--- Models/VideoGames.cs
+++ Models/VideoGames.cs
@@ -5,8 +5,8 @@ namespace VideoGameCatalogue.Api.Models
     public class VideoGame
     {
         public Guid Id { get; set; }
-        public string Title { get; set; }
         [Required]
+        public string Title { get; set; }        
         public string Platform { get; set; }
         public string Genre { get; set; }
         public DateTime ReleaseDate { get; set; }
